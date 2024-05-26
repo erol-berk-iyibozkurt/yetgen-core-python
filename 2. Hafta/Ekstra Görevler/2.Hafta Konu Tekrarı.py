26dd900def2996ded07062880b1ca4ba777b7b27
erol-berk-iyibozkurt <biyibozkurt@gmail.com>
2024-05-26 19:04:49 +0200

    2. Hafta 3.Bölüm
    değişkenler, matematiksel işlemler ve input fonksiyonu gibi konuları işledim

---
+++ 2. Hafta/Ekstra Görevler/2.Hafta Konu Tekrarı.py
@@ -0,0 +1,1 @@
+# 2. Haftada öğrendiğim bütün konuları bu dosyada kullanmaya çalışacağım. Bunu yaparken tüm konuları bir arada kullanarak projemsi bir dosya oluşturmuş olacağım. Amacım tüm öğrendiklerimin pekişmesini sağlamak.
